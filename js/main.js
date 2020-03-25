c8eec682d84b4ad237573323191f3fe634bfc29f
znugab <abintang07@gmail.com>
2020-03-26 04:15:54 +0700

    JavaScript Version 1.0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
-$(document).ready(function () {
-    $('.header').height($(window).height());
+$(document).ready(function() {
+
+    var toggleAffix = function(affixElement, scrollElement, wrapper) {
+  
+        var height = affixElement.outerHeight(),
+            top = wrapper.offset().top;
+        
+        if (scrollElement.scrollTop() >= (top - height)){
+            // wrapper.height(height);
+            affixElement.addClass("affix");
+        }
+        else {
+            affixElement.removeClass("affix");
+        }
+      
+    };
+  
+
+    $('[data-toggle="affix"]').each(function() {
+        var ele = $(this),
+            wrapper = $('<div></div>');
+        
+        ele.before(wrapper);
+        $(window).on('scroll resize', function() {
+            toggleAffix(ele, $(this), wrapper);
+        });
+        
+        // init
+        toggleAffix(ele, $(window), wrapper);
+    });
+  
 });
